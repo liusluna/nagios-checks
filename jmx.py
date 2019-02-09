897748cf72fe057afb50b21cb6770f6e6a0257cc
liusluna <lius.luna@gmail.com>
2019-02-09 12:50:36 -0600

    Check PermGen
    Check PermGen, 
    cambio serverRuntime() por domainRuntime(), ahora funciona en servidores con un dominio

--- jmx.py
+++ jmx.py
@@ -14,40 +14,50 @@ redirect('/dev/null', 'false')
 
 if authType == "KEY":
 	connect(userConfigFile=userName, userKeyFile=passWord, url=srvurl)
-	serverRuntime()
+	#serverRuntime()
 elif authType == "USRPW":
 	connect(userName, passWord, url=srvurl)
-	serverRuntime()
+	#serverRuntime()
 else:
 	print "No Connection"
 
 state = sys.argv[3]
 mgserver = sys.argv[4]
 
-
+if state =="PermGen":
+        domainCustom()
+        cd('java.lang/java.lang:Location='+mgserver+',name=Perm Gen,type=MemoryPool')
+        hstate = get('CollectionUsage')
+        print state + ":"+`hstate`
+        exit()
 if state == "HealthState":
+	domainRuntime()
 	hstate = get(state)
 	print state + ":"+`hstate`
 	##discconect()
 	exit()
 elif state == "OverallHealthState":
+	domainRuntime()
 	hstate = get(state)
 	print state + ":"+`hstate`
 	##discconect()
 	exit()
 elif state == "ThreadHealthState":
+	domainRuntime()
 	cd('/ThreadPoolRuntime/ThreadPoolRuntime')
 	hstate = get('HealthState')
 	print state + ":"+`hstate`
 	##discconect()
 	exit()
 elif state == "JDBCHealthState":
+	domainRuntime()
 	cd('/JDBCServiceRuntime/' + mgserver)
 	hstate = get('HealthState')
 	print state + ":"+`hstate`
 	##discconect()
 	exit()
 elif state == "HeapFreePercent":
+	domainRuntime()
 	cd('/JVMRuntime/' + mgserver)
 	hpercent = int(get('HeapFreePercent'))
 	##print 'HeapFreePercent:'+ `hpercent`
@@ -55,11 +65,13 @@ elif state == "HeapFreePercent":
 	##disconnect()
 	exit()
 elif state == "StuckThreadCount":
+	domainRuntime()
 	cd('/ThreadPoolRuntime/ThreadPoolRuntime')
 	hsthrd = int(get('StuckThreadCount'))
-	print state + ":"+`hsthrd`
+	print state + ":"+`hsthrd`:wq
 	exit()
 elif state == "ThreadTotCount":
+	domainRuntime()
 	cd('/ThreadPoolRuntime/ThreadPoolRuntime')
 	hthrdcnt = int(get('ExecuteThreadTotalCount'))
 	print state + ":"+`hthrdcnt`
